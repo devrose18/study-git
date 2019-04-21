9fc73c2e0de26a5010797e551aaf275682fafef5
devrose18 <dev_rose18@163.com>
2019-04-21 22:07:04 +0800

    diyicitjiao

---
+++ src/cn/miss/core/web/login.java
@@ -0,0 +1,8 @@
+package cn.miss.core.web;
+
+public class login {
+	
+	public static void main(String[] args) {
+		System.out.println("start to login");
+	}
+}
---
+++ src/com/example/demo/controller/TestController.java
@@ -0,0 +1,8 @@
+package com.example.demo.controller;
+
+public class TestController {
+	
+	public String test(String username, String password) {
+		return "springboot测试成功";
+	}
+}
